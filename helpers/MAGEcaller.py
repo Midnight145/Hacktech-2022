97eb53f4d66d643a063a1a6e3bdbc3277bc904e2
Kratargon <lmdavis@caltech.edu>
2022-03-05 03:06:30 -0800

    Fixed dictionary syntax
    Updated syntax to correctly reference dictionary key-object pairs

--- helpers/MAGEcaller.py
+++ helpers/MAGEcaller.py
@@ -8,12 +8,12 @@ def getResponse(MAGEinfo, lastMinute):
             'Content-Type': 'application/json',
         },
         data=json.dumps({
-            'api_key': MAGEinfo.apiKey,
-            'model': MAGEinfo.model,
-            'version': MAGEinfo.version,
+            'api_key': MAGEinfo['apiKey'],
+            'model': MAGEinfo['model'],
+            'version': MAGEinfo['version'],
             'features': [lastMinute], # lastMinute is of type Dict
         }),
     )
 
     predictions = response.json()
-    return(predictions.prediction)
+    return(predictions['prediction'])
