682146128754b3a1a2c2eea3052a17b1b36ee2a9
Kratargon <lmdavis@caltech.edu>
2022-03-05 02:59:19 -0800

    First draft of MAGEcaller getResponse
    Fixes file to be contained within a function, returning the predicted recognized behavior.

--- helpers/MAGEcaller.py
+++ helpers/MAGEcaller.py
@@ -1,22 +1,19 @@
 import json
 import requests
 
+def getResponse(MAGEinfo, lastMinute):
+    response = requests.post(
+        url='https://api.mage.ai/v1/predict',
+        headers={
+            'Content-Type': 'application/json',
+        },
+        data=json.dumps({
+            'api_key': MAGEinfo.apiKey,
+            'model': MAGEinfo.model,
+            'version': MAGEinfo.version,
+            'features': [lastMinute], # lastMinute is of type Dict
+        }),
+    )
 
-response = requests.post(
-    url='https://api.mage.ai/v1/predict',
-    headers={
-        'Content-Type': 'application/json',
-    },
-    data=json.dumps({
-        'api_key': 'abc_123',
-        'model': 'awesome_house_predictor',
-        'version': '1',
-        'features': [{
-            'floor_count': 4,
-            'year_built': 1967,
-        }],
-    }),
-)
-
-predictions = response.json()
-print(predictions)
+    predictions = response.json()
+    return(predictions.prediction)
