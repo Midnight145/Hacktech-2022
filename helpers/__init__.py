9260e8e69d430152b2a028902a1f69febc746ce5
Midnight <rubikscubeboy27@gmail.com>
2022-03-05 14:53:13 -0600

    commit

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,5 +4,4 @@ KEY_HEADERS = list(string.printable[:-6])
 KEY_HEADERS += ["esc", "enter", "delete", "ctrl", "left", "up", "right", "down", "backspace", "tab", "space", "windows", "alt", "shift"]
 KEY_HEADERS.append('state')
 
-MOUSE_HEADERS = ["left_single", "left_double", "right_single", "right_double", "middle_single", "middle_double",
-                 "x_single", "x_double", "x2_single", "x2_double", "state"]
+MOUSE_HEADERS = ["left", "right", "middle", "x1", "x2"]
