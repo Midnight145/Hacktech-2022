aeaf88e05a646ceae7860afb6528515eef1b9ec0
Kratargon <lmdavis@caltech.edu>
2022-03-06 03:38:59 -0800

    snake_case :(

--- helpers/MAGEcaller.py
+++ helpers/MAGEcaller.py
@@ -1,18 +1,18 @@
 import json
 import requests
 
-MAGE_HEADERS = { 'Content-Type': 'application/json' }
+MAGE_HEADERS = {'Content-Type': 'application/json'}
 
 
-def call_api(mageInfo, lastMinute):
+def call_api(mage_info, last_minute):
     response = requests.post(
         url='https://api.mage.ai/v1/predict',
         headers=MAGE_HEADERS,
         data=json.dumps({
-            'api_key': mageInfo['apiKey'],
-            'model': mageInfo['model'],
-            'version': mageInfo['version'],
-            'features': [lastMinute], # lastMinute is of type Dict
+            'api_key': mage_info['apiKey'],
+            'model': mage_info['model'],
+            'version': mage_info['version'],
+            'features': [mage_info],  # lastMinute is of type Dict
         }),
     )
 
